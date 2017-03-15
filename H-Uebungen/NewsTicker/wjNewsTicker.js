406990942a7eb6a3d66df3f92bf8dfd3f651b2af
josef5526 <WeinhapplJ@wifi-it.at>
2017-03-15 08:48:22 +0100

    Woche4-HÜ

---
+++ H-Uebungen/NewsTicker/wjNewsTicker.js
@@ -0,0 +1,93 @@
+﻿let filler = '<span>  + + +  </span>';
+var wjTicker = {
+	html:{},
+	$html:{},
+	footer:{},
+	newsTicker:'',
+	$body:{},
+	$head:{},
+
+	// fetch title and href for ticker	
+	handleOneNewsEntry:function( ind, aentry ){
+		var href = $( aentry ).attr( 'href' );
+		var title = $( aentry ).find( 'h1' ).html();
+		wjTicker.addToNewsTicker( href,title );
+	},
+	
+	// add html for given title and href to ticker-html
+	addToNewsTicker:function( href, title ){
+		wjTicker.newsTicker += '<a href=\'' + href + '\'>' + title + '</a>' + filler;
+	},
+	
+	// control newsTicker preparation and insertion
+	fetchNews:function( inHTML ){
+		wjTicker.initNewsTicker();
+		var $newsDiv = $( 'div.grid.news', wjTicker.$body );   // fetch elements containing the news 
+		var news = $( $newsDiv.find( 'a' ) );                  // accumulate the elements (all are links)
+		$.each(news, wjTicker.handleOneNewsEntry );            // add to ticker html
+		wjTicker.modifyPage();								   // add ticker to loaded page (in the footer)
+		wjTicker.makeFooter();								   // add ticker to own footer
+	},
+	
+	initNewsTicker: function(){
+		wjTicker.newsTicker = filler;
+	},
+	
+	// load page into iFrame container
+	insertHTML:function( inHTML ){
+		var doc = document.getElementById('container').contentWindow.document;   // get document object of iFrame
+		try{
+			doc.open();					// open document
+			doc.write( inHTML );		// write loaded page
+			doc.close();				// close it
+			wjTicker.$body = $(  $('body', $( doc )) );  // get body of loaded page
+			wjTicker.$head = $(  $('head', $( doc )) );	 // get head of loaded page
+			$( $( '#container' ) ).ready( function(){	 // when loaded page ready		
+				wjTicker.$body = $(  $('body', $( doc )) );  // do it again
+				wjTicker.$head = $(  $('head', $( doc )) );
+			});
+		}catch (error){
+			// loading page from server causes exception : $ undefined within loaded page.
+			// downt know why. Continuation is possible.
+		}
+		finally{
+			wjTicker.$body = $(  $('body', $( doc )) ); // to be sure 
+			wjTicker.$head = $(  $('head', $( doc )) );
+			wjTicker.fetchNews();                       // start ticker setup
+		}
+	},
+	
+	// insert ticker into footer of loaded page
+	modifyPage:function(){
+		// add ticker-css to loaded page
+		var $css = $( $( '<link href="wjNewsTicker-styles.css" rel="stylesheet" type="text/css">' ) );
+		$( wjTicker.$head ).append( $css );
+		// get footer
+		var $footer = $( 'footer', wjTicker.$body);
+		// clear footer
+		$footer.html( '' );
+		// create ticker element and add it to footer
+		$( '<div class="marquee">' ).html( wjTicker.newsTicker ).appendTo( $footer );
+	},
+	
+	// insert ticker also in own footer
+	makeFooter:function(){
+		$( 'footer' ).html( '' );
+		$( '<div class="marquee">' ).html( wjTicker.newsTicker ).appendTo( $( 'footer' ) );
+	},
+		
+	// load page from server
+	loadPage:function(){
+ 	   $.ajax({
+				dataType:'text',
+				url:'http://wifi.1av.at/516/tn/news.php',
+				success:function( data ){
+					wjTicker.insertHTML( data );
+				}
+		   }); 
+	}
+}
+
+$( document ).ready( function(){
+	wjTicker.loadPage();
+});
---
+++ H-Uebungen/Würfel-Maus/würfel1.js
@@ -0,0 +1,91 @@
+﻿let oben = 0;
+let vorne = 1;
+let unten = 2;
+let hinten = 3;
+let links = 4;
+let rechts = 5;
+// shift = 16
+// strg = 17
+let extendLeftRight = 17;
+
+var cube = {
+	sides:[2,1,5,6,3,4],
+	
+	dumpCube:function(){
+		var text = 'V:'+cube.sides[vorne]+', O:'+cube.sides[oben] + ', U:' + cube.sides[unten];
+		text += ', L:'+cube.sides[links] + ', H:' + cube.sides[hinten] + ', R:'+ cube.sides[rechts];
+		console.log( text );
+	},
+		
+	cubeHandler:function( direction, shift ){
+		var lastVorne = cube.sides[vorne];
+		var newVorne;
+		var help;
+		switch ( direction ){
+		case 38 : // up
+		case 40 : // down
+			if (direction == 38){
+				cube.sides[oben] = cube.sides[vorne];
+				cube.sides[vorne] = cube.sides[unten];
+			} else {
+				cube.sides[vorne] = cube.sides[oben];
+				cube.sides[oben] = cube.sides[hinten];
+			}
+			cube.sides[unten] = 7 - cube.sides[oben];
+			cube.sides[hinten] = 7 - cube.sides[vorne];
+			newVorne = cube.sides[vorne];
+			break;
+		case 37 : // left
+		case 39 : // right
+			if (shift){
+				if (direction == 37){ // shift + left
+					cube.sides[unten] = cube.sides[links];
+					cube.sides[links] = cube.sides[oben];
+				} else {    // shift + right
+					cube.sides[links] = cube.sides[unten];
+					cube.sides[unten] = cube.sides[rechts];
+				}
+				cube.sides[oben] = 7 - cube.sides[unten];
+				cube.sides[rechts] = 7 - cube.sides[links];
+				return;
+			} else {
+				if (direction == 37){ // links
+					cube.sides[links] = cube.sides[vorne];
+					cube.sides[vorne] = cube.sides[rechts];
+				} else {  // rechts
+					cube.sides[vorne] = cube.sides[links];
+					cube.sides[links] = cube.sides[hinten];
+				}
+				cube.sides[rechts] = 7 - cube.sides[links];
+				cube.sides[hinten] = 7 - cube.sides[vorne];
+				newVorne = cube.sides[vorne];
+			}
+			break;
+		default:
+			return;
+		}
+		$( '[data-ind="'+lastVorne+'"]' ).hide();
+		$( '[data-ind="'+newVorne+'"]' ).show();
+	},
+	
+	arrowClick:function( e ){
+		e.preventDefault();
+		var dir = $( this ).attr( 'data-ind' ) * 1;
+		cube.cubeHandler( dir, false );
+		cube.dumpCube();
+	},
+	
+	sArrowClick:function( e ){
+		e.preventDefault();
+		var dir = $( this ).attr( 'data-ind' ) * 1;
+		cube.cubeHandler( dir, true );
+		cube.dumpCube();
+	}
+}
+
+$( document ).ready( function() {
+	$( '.arrow,.middleArrow' ).on( 'click', cube.arrowClick );
+	$( '.sArrow' ).on( 'click', cube.sArrowClick );
+	cube.dumpCube();
+//	$( document ).on( 'keyup', cube.keyupHandler );
+});
